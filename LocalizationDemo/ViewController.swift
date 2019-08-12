b48b974a636fa5820015a3df170e5a793c005a86
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-08-12 12:29:18 +0530

    L10N - Setup Project
    - Did project setup
    - Added support of English(Base), French and Itanlian language.

---
+++ LocalizationDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  LocalizationDemo
+//
+//  Created by GovindMulgir on 09/08/19.
+//  Copyright © 2019 GovindMulgir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let greetLabel = UILabel(frame: CGRect(x: 10, y: 50, width: view.bounds.width - 20 , height: 50))
+        greetLabel.backgroundColor = .red
+        greetLabel.textAlignment = .center
+        greetLabel.text = NSLocalizedString("greet-message-key", comment: "Not used")
+        
+        view.addSubview(greetLabel)
+    }
+}
+
