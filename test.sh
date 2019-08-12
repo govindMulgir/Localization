7b809972001fba7e256d9a7bdcbddc675412232b
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-08-12 21:37:38 +0530

    test.sh updated

--- test.sh
+++ test.sh
@@ -4,10 +4,12 @@ echo "Custom script - start"
 git config --global user.name "govindMulgir"
 git config --global user.email "govind.mulgir@rajasoftwarelabs.com"
 
+agvtool next-version -all
+
 git add "LocalizationDemo.xcodeproj/project.pbxproj"
 git add "LocalizationDemo/Info.plist"
 git commit -m "upgraded build version"
-git push
+git push origin HEAD:master
 
 git config --global user.name "test"
 git config --global user.email "test@gmail.com"
