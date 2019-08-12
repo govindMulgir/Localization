a990aa6bc85563bbe7b62e2b144c37658751bc28
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-08-12 21:44:18 +0530

    test.sh updated2

--- test.sh
+++ test.sh
@@ -9,6 +9,9 @@ agvtool next-version -all
 git add "LocalizationDemo.xcodeproj/project.pbxproj"
 git add "LocalizationDemo/Info.plist"
 git commit -m "upgraded build version"
+
+git remote add origin https://github.com/govindMulgir/Localization.git
+
 git push origin HEAD:master
 
 git config --global user.name "test"
