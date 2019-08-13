78f81ba521207c53e55a76365511f8c5127fc588
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-08-13 10:59:58 +0530

    Checkout to master branch command code added

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 echo "Custom script - start"
 
-git config --global user.name "govindMulgir"
-git config --global user.email "govind.mulgir@rajasoftwarelabs.com"
+#git config --global user.name "govindMulgir"
+#git config --global user.email "govind.mulgir@rajasoftwarelabs.com"
+
+echo "Custom - checking out to master branch"
+
+git checkout master
+
+echo "Custom - you are on master branch"
 
 agvtool next-version -all
 
@@ -10,11 +16,11 @@ git add "LocalizationDemo.xcodeproj/project.pbxproj"
 git add "LocalizationDemo/Info.plist"
 git commit -m "upgraded build version"
 
-git remote add origin https://github.com/govindMulgir/Localization.git
+#git remote add origin https://github.com/govindMulgir/Localization.git
 
-git push origin HEAD:master
+git push
 
-git config --global user.name "test"
-git config --global user.email "test@gmail.com"
+#git config --global user.name "test"
+#git config --global user.email "test@gmail.com"
 
 echo "Custom script - end"
