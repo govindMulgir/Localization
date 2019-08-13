7e22a70bab4d8dba59a5fa92eb6553bfda9dadd0
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-08-13 11:26:51 +0530

    test.sh modified

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 echo "Custom script - start"
 
-#git config --global user.name "govindMulgir"
-#git config --global user.email "govind.mulgir@rajasoftwarelabs.com"
+git config --global user.name "govindMulgir"
+git config --global user.email "govind.mulgir@rajasoftwarelabs.com"
 
 echo "Custom - checking out to master branch"
 
@@ -12,9 +12,18 @@ echo "Custom - you are on master branch"
 
 agvtool next-version -all
 
+echo "Custom - git status => start"
+git status
+echo "Custom - git status => end"
+
 git add "LocalizationDemo.xcodeproj/project.pbxproj"
 git add "LocalizationDemo/Info.plist"
+
+echo "Custom - git commit => start"
+
 git commit -m "custom - upgraded build version [skip ci]"
+
+echo "Custom - git commit => end"
 
 #git remote add origin https://github.com/govindMulgir/Localization.git
 
