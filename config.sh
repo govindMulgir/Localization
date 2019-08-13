2bc140c0587ab16e93900fa084163c1ab9a3a987
Govind Mulgir <govind.mulgir@rajasoftwarelabs.com>
2019-08-13 11:39:03 +0530

    modified config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 echo "custom - configured git =>  start"
 
-#git config --global user.name "govindMulgir"
-#git config --global user.email "govind.mulgir@rajasoftwarelabs.com"
+git config --global user.name "govindMulgir"
+git config --global user.email "govind.mulgir@rajasoftwarelabs.com"
 
 echo "custom - configured git =>  end"
